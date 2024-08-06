17b39bc3b66cccf44dedb1d89507964f44a2e2cd
Faiz <ahmadfaiz21@outlook.com>
2024-08-06 17:11:56 +0800

    Hampir Complete

---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModernDesign.Core;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            CurrentView = HomeVm;
+        }
+    }
+}
