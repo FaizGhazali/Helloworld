a3be22686c39074f5cece98c7f35517f2b2fc520
Faiz Ghazali <99084931+FaizGhazali@users.noreply.github.com>
2024-08-07 21:06:06 +0800

    .

--- ModernDesign/obj/Release/net6.0-windows/App.g.cs
+++ ModernDesign/obj/Release/net6.0-windows/App.g.cs
@@ -49,7 +49,7 @@ namespace ModernDesign {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -75,7 +75,7 @@ namespace ModernDesign {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
         public static void Main() {
             ModernDesign.App app = new ModernDesign.App();
             app.InitializeComponent();
--- ModernDesign/obj/Release/net6.0-windows/App.g.i.cs
+++ ModernDesign/obj/Release/net6.0-windows/App.g.i.cs
@@ -49,7 +49,7 @@ namespace ModernDesign {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -61,7 +61,7 @@ namespace ModernDesign {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/ModernDesign;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ModernDesign;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -75,7 +75,7 @@ namespace ModernDesign {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
         public static void Main() {
             ModernDesign.App app = new ModernDesign.App();
             app.InitializeComponent();
