1593ad028b196986e4c7df4b1cca1f6dc8a3b256
Faiz Ghazali <99084931+FaizGhazali@users.noreply.github.com>
2024-08-08 20:58:58 +0800

    Add Menu Radio For Progamming

--- ModernDesign/MVVM/ViewModel/PracticeRadioViewModel.cs
+++ ModernDesign/MVVM/ViewModel/PracticeRadioViewModel.cs
@@ -9,25 +9,21 @@ namespace ModernDesign.MVVM.ViewModel
 {
     public class PracticeRadioViewModel
     {
-        public RelayCommand HMViewCommand { get; set; }
-        
+        public RelayCommand ChangeViewCommand { get; set; }
 
         public PracticeRadioViewModel()
         {
-            HMViewCommand = new RelayCommand(o => RequestViewChange());
+            ChangeViewCommand = new RelayCommand(o => RequestViewChange());
         }
 
         public void RequestViewChange()
         {
-            DiscoveryViewModel DVM = GetExistingViewModel();
             Messenger.Instance.Send(new ChangeViewMessage { ViewModel = new DiscoveryViewModel() });
         }
-        private DiscoveryViewModel GetExistingViewModel()
-        {
-            // Logic to get the existing view model instance
-            // This can be a field, property, or a method that returns the instance
-            return someExistingViewModelInstance;
-        }
 
+        public void Trigger()
+        {
+            RequestViewChange();
+        }
     }
 }
