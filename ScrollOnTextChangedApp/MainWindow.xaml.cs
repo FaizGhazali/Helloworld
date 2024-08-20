f657b5235533c0e95a0dddd377fcf409c949bb0e
Faiz Ghazali Desktop <99084931+FaizGhazali@users.noreply.github.com>
2024-08-20 11:54:04 +0800

    .

---
+++ ScrollOnTextChangedApp/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace ScrollOnTextChangedApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
